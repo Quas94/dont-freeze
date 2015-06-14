570d6345c7cd722c34db3812aa7f34637c89c119
Quasar <quasar.exe@hotmail.com>
2015-06-14 14:59:05 +1000

    LibGDX/gradle files added. Project runnable showing empty 640x480 window.

---
+++ desktop/src/com/tbd/dontfreeze/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.tbd.dontfreeze.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tbd.dontfreeze.GameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 480;
+		config.width = 640;
+		config.title = "pls don't freeze bby";
+		new LwjglApplication(new GameMain(), config);
+	}
+}
